a4c182fc492d3c6e2d16c4db1208af5d5993d6e1
Jordan <g0983230881@gmail.com>
2025-07-19 19:41:43 +0800

    update Legacy Architecture to Fabric(new Architecture)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-webview)/)',
+  ],
+  moduleNameMapper: {
+    '^react-native-gesture-handler$': '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
+    '^@react-native-async-storage/async-storage$': '<rootDir>/__mocks__/@react-native-async-storage/async-storage.js',
+  },
 };
